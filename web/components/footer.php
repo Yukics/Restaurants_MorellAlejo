49a45294caa0a767b19d3d2ecc10193831f14434
Yukics <alejomorell@gmail.com>
2022-04-04 20:26:08 +0200

    style: :art: bg-light footer

--- web/components/footer.php
+++ web/components/footer.php
@@ -1,5 +1,5 @@
 <?php
     function printFooter(){
-        return '<footer style="margin-bottom: 0%; padding-top:1%; margin-top 10%" class="fixed-bottom"><p class="text-center">Copyright 2022 Alejo Morell</p></footer>';
+        return '<footer style="margin-bottom: 0%; padding-top:1%; margin-top 10%" class="fixed-bottom bg-light"><p class="text-center">Copyright 2022 Alejo Morell</p></footer>';
     }
 ?>
